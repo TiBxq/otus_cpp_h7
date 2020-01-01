2d929d45138f3227e87dc6108ba0d46b484e3261
Bogdan 'TiBx' Savenko <savenko.bogdan@gmail.com>
2020-01-01 17:45:10 +0200

    added empty project

---
+++ bulk.cpp
@@ -0,0 +1,11 @@
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main(/*int argc, char const *argv[]*/)
+{
+    return 0;
+}
---
+++ bulk_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+TEST(bulk, bulk_test1)
+{
+    ASSERT_TRUE(true);
+}
+
+int main(int argc, char *argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
