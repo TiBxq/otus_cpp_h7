8135a3f7cbdd742d5f6753a31e73bf2c59011f35
Bogdan 'TiBx' Savenko <savenko.bogdan@gmail.com>
2020-01-03 01:51:31 +0200

    added tests

--- bulk_test.cpp
+++ bulk_test.cpp
@@ -1,8 +1,58 @@
 #include <gtest/gtest.h>
 
+#include "CommandMgr.h"
+#include "CommandPackage.h"
+#include "Command.h"
+
 TEST(bulk, bulk_test1)
 {
-    ASSERT_TRUE(true);
+    CommandMgr mgr(3);
+    StringCommandHandler* strHandler = new StringCommandHandler();
+    mgr.AddHandler(strHandler);
+
+    mgr.Add(new Command("cmd1"));
+    mgr.Add(new Command("cmd2"));
+    mgr.Add(new Command("cmd3"));
+
+    ASSERT_EQ(strHandler->str(), "bulk: cmd1, cmd2, cmd3\n");
+}
+
+TEST(bulk, bulk_test2)
+{
+    CommandMgr mgr(3);
+    StringCommandHandler* strHandler = new StringCommandHandler();
+    mgr.AddHandler(strHandler);
+
+    mgr.OpenBlock();
+    mgr.Add(new Command("cmd1"));
+    mgr.Add(new Command("cmd2"));
+    mgr.Add(new Command("cmd3"));
+    mgr.Add(new Command("cmd4"));
+    mgr.Add(new Command("cmd5"));
+    mgr.CloseBlock();
+
+    ASSERT_EQ(strHandler->str(), "bulk: cmd1, cmd2, cmd3, cmd4, cmd5\n");
+}
+
+TEST(bulk, bulk_test3)
+{
+    CommandMgr mgr(3);
+    StringCommandHandler* strHandler = new StringCommandHandler();
+    mgr.AddHandler(strHandler);
+
+    mgr.OpenBlock();
+    mgr.Add(new Command("cmd1"));
+    mgr.OpenBlock();
+    mgr.Add(new Command("cmd2"));
+    mgr.OpenBlock();
+    mgr.Add(new Command("cmd3"));
+    mgr.Add(new Command("cmd4"));
+    mgr.CloseBlock();
+    mgr.CloseBlock();
+    mgr.Add(new Command("cmd5"));
+    mgr.CloseBlock();
+
+    ASSERT_EQ(strHandler->str(), "bulk: cmd1, cmd2, cmd3, cmd4, cmd5\n");
 }
 
 int main(int argc, char *argv[])
