81925a9542f67531298f88a9b76ac29a0de0e8ab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-03 00:55:32 +0000

    Deploy TiBxq/otus_cpp_h7 to github.com/TiBxq/otus_cpp_h7.git:gh-pages

--- search/all_a.js
+++ search/all_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['otus_5fcpp_5fh7',['otus_cpp_h7',['../md_README.html',1,'']]],
   ['oncompleted',['OnCompleted',['../class_command_mgr.html#ade2efd944c6a552cbf28fb2979d6a89a',1,'CommandMgr']]],
+  ['oneof',['OnEOF',['../class_command_mgr.html#a0c0546c0fd7fda55a5ebfeb002ecd695',1,'CommandMgr']]],
   ['openblock',['OpenBlock',['../class_command_mgr.html#acfed27d80d3f9650343cc9b7db2020c9',1,'CommandMgr']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['oncompleted',['OnCompleted',['../class_command_mgr.html#ade2efd944c6a552cbf28fb2979d6a89a',1,'CommandMgr']]],
+  ['oneof',['OnEOF',['../class_command_mgr.html#a0c0546c0fd7fda55a5ebfeb002ecd695',1,'CommandMgr']]],
   ['openblock',['OpenBlock',['../class_command_mgr.html#acfed27d80d3f9650343cc9b7db2020c9',1,'CommandMgr']]]
 ];
